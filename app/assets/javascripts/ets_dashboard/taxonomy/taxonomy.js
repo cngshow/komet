3e09952d0329cf98befe62f36291211a1cc4e758
TSWhalen <tim.whalen@mantech.com>
2015-12-30 04:35:15 -1000

    Added changes to create reverse tree and display a message in the content pane when a tree item is selected

--- app/assets/javascripts/ets_dashboard/taxonomy/taxonomy.js
+++ app/assets/javascripts/ets_dashboard/taxonomy/taxonomy.js
@@ -17,22 +17,33 @@ var TaxonomyModule = (function () {
         $(jstree_id).jstree(settings);
         $(jstree_id).on('after_open.jstree', onAfterOpen);
         $(jstree_id).on('after_close.jstree', onAfterClose);
+        $(jstree_id).on('changed.jstree', onChanged);
         TaxonomyModule.tree = $(jstree_id).jstree();
     }
 
-     function onAfterOpen(node, selected) {
+    function onAfterOpen(node, selected) {
         //publish what we expect our observers to need in a way that allows them not to understand
         //our tree and our tree's dom.
         $.publish(EtsChannels.Taxonomy.taxonomyTreeNodeOpenedChannel,selected.node.id);
 
     }
 
-     function onAfterClose(node, selected) {
+    function onAfterClose(node, selected) {
         $.publish(EtsChannels.Taxonomy.taxonomyTreeNodeClosedChannel,selected.node.id);
     }
 
     function init(jstree_id, rest_paths) {
         buildTaxonomyTree(jstree_id, rest_paths);
+    }
+
+    function onChanged(event, selectedObject) {
+
+        var conceptId = selectedObject.node.id;
+        var parentConceptId = selectedObject.node.original.parent_id;
+        console.log("Selected id : %s; Parent id: %s", conceptId, parentConceptId);
+
+        var contentPane = $("#east-pane");
+        contentPane.html("Viewing details for concept ID: " + conceptId + "; which has parent concept ID: " + parentConceptId);
     }
 
     return {
@@ -44,39 +55,39 @@ var TaxonomyModule = (function () {
 
 
 /*
-var taxonomy = new function () {
-    //public methods (defined as this. will be exposed in the taxonomy namespace)
-    this.init = function (jstree_id, rest_paths) {
-        $(jstree_id)
-            .on('changed.jstree', function (e, data) {
-                console.log("******** on changed of tree " + JSON.stringify(e));
-            })
-            .on('before_open.jstree', function (node) {
-                console.log("******** before open on " + node);
-            })
-            .on('open_node.jstree', function (node) {
-                console.log("******** open_node on" + node);
-            })
-            .on('after_open.jstree', function (node) {
-                console.log("******** after open on " + node);
-            })
-            // create the instance
-            .jstree({
-                "core": {
-                    "animation": 0,
-                    "check_callback": true,
-                    "themes": {"stripes": true},
-                    'data': {
-                        'url': rest_paths.taxonomy_data_path,
-                        'data': function (node) {
-                            return {'id': node.id};
-                        }
-                    }
-                }
-            });
-    };
+ var taxonomy = new function () {
+ //public methods (defined as this. will be exposed in the taxonomy namespace)
+ this.init = function (jstree_id, rest_paths) {
+ $(jstree_id)
+ .on('changed.jstree', function (e, data) {
+ console.log("******** on changed of tree " + JSON.stringify(e));
+ })
+ .on('before_open.jstree', function (node) {
+ console.log("******** before open on " + node);
+ })
+ .on('open_node.jstree', function (node) {
+ console.log("******** open_node on" + node);
+ })
+ .on('after_open.jstree', function (node) {
+ console.log("******** after open on " + node);
+ })
+ // create the instance
+ .jstree({
+ "core": {
+ "animation": 0,
+ "check_callback": true,
+ "themes": {"stripes": true},
+ 'data': {
+ 'url': rest_paths.taxonomy_data_path,
+ 'data': function (node) {
+ return {'id': node.id};
+ }
+ }
+ }
+ });
+ };
 
-    //private methods should be defined as var <function_name> = function(...)
-};
+ //private methods should be defined as var <function_name> = function(...)
+ };
 
-*/
+ */
