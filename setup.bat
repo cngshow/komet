e453d783fd149e5564882a0e41e09d9ea19fe4e8
TSWhalen <tim.whalen@mantech.com>
2015-12-30 08:54:32 -1000

    Canceling changes I commited by mistake

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 set JRUBY_OPTS=-J-Xmx2g
-set GEM_HOME=C:\Users\tswhalen\Documents\tooling\ets_tooling\gem_home
-set JRUBY_JAR=C:\jruby-9.0.4.0\jruby-complete-9.0.4.0.jar
-set JRUBY_PATH=C:\jruby-9.0.4.0
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_66
+set GEM_HOME=C:\work\ETS\gem_home
+set JRUBY_JAR=%JRUBY_COMPLETE_JAR%
+set JRUBY_PATH=C:\languages\jruby\jruby-9.0.4.0
+set JAVA_HOME=C:\languages\Java\jdk1.8.0_65
 set PATH=%GEM_HOME%\bin;%JRUBY_PATH%\bin;%JAVA_HOME%\bin;%PATH%;
