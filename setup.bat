0aaa181083eea17992aa542579bef94749149faa
cshupp <cshupp@gmail.com>
2016-01-05 15:32:19 -0500

    Changing setup.bat to setup.bat.template

--- setup.bat
+++
@@ -1,6 +0,0 @@
-set JRUBY_OPTS=-J-Xmx2g
-set GEM_HOME=C:\work\ETS\gem_home
-set JRUBY_PATH=C:\languages\ruby\jruby-9.0.4.0
-set JRUBY_JAR=%JRUBY_PATH%\jruby-complete-9.0.4.0.jar
-set JAVA_HOME=C:\languages\Java\jdk1.8.0_65
-set PATH=%GEM_HOME%\bin;%JRUBY_PATH%\bin;%JAVA_HOME%\bin;%PATH%;
