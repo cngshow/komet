ec08d23cfd5dcb303751daa8f07a24d6de096ba1
gpbowman-git <gpbowman-git@gmail.com>
2016-01-21 16:56:49 -0500

    refactored rake tasks moving devops to ets_common

--- Rakefile
+++ Rakefile
@@ -2,5 +2,5 @@
 # for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
 
 require File.expand_path('../config/application', __FILE__)
-
+Rake.add_rakelib('./lib/ets_common/tasks')
 Rails.application.load_tasks
--- config/initializers/01_ets_init.rb
+++ config/initializers/01_ets_init.rb
@@ -1,6 +1,6 @@
 ##
 # Do common initilization tasks in ets tooling
 #
-require './lib/ets_common/prop_loader'
-require './lib/ets_common/open_logging'
-require './lib/ets_common/logging'
+require './lib/ets_common/props/prop_loader'
+require './lib/ets_common/logging/open_logging'
+require './lib/ets_common/logging/logging'
