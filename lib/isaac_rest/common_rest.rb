51e1793de81ed5127e3d1a5aac8ddd566a10e9c5
gpbowman-git <gpbowman.git@gmail.com>
2016-03-15 14:40:45 -0400

    fixed misspelling

--- lib/isaac_rest/common_rest.rb
+++ lib/isaac_rest/common_rest.rb
@@ -18,7 +18,7 @@ module CommonRest
 
   def uuid_check(uuid:)
     if uuid.nil?
-      $log.error('The UUID cannot be nil!  Please esure the caller provides a UUID.')
+      $log.error('The UUID cannot be nil!  Please ensure the caller provides a UUID.')
       raise ArgumentError.new('The UUID cannot be nil!!')
     end
   end
