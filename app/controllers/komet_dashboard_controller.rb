2810efc480f455e24891831aea254d865d70ba4c
cshupp <cshupp@gmail.com>
2016-05-02 12:24:59 -0400

    Prisme needed some Komet code.  Moving that code to Rails Common.

--- app/controllers/komet_dashboard_controller.rb
+++ app/controllers/komet_dashboard_controller.rb
@@ -18,12 +18,14 @@ Copyright Notice
 =end
 
 require './lib/isaac_rest/taxonomy_rest'
+require './lib/rails_common/util/controller_helpers'
 
 ##
 # KometDashboardController -
 # handles the loading of the taxonomy tree
 class KometDashboardController < ApplicationController
   include TaxonomyConcern, ConceptConcern, InstrumentationConcern, ISAACConstants
+  include CommonController
 
   before_action :setup_routes, :setup_constants, :only => [:dashboard]
   after_filter :byte_size unless Rails.env.production?
@@ -314,35 +316,6 @@ class KometDashboardController < ApplicationController
     @stated = 'true'
   end
 
-  def setup_routes
-    routes = Rails.application.routes.named_routes.helpers.to_a
-    routes_hash = {}
-    routes.each do |route|
-      begin
-        routes_hash[route.to_s] = self.send(route)
-      rescue ActionController::UrlGenerationError => ex
-        if (ex.message =~ /missing required keys: \[(.*?)\]/)
-          keys = $1
-          keys = keys.split(',')
-          keys.map! do |e|
-            e.gsub!(':', '')
-            e.strip
-          end
-          required_keys_hash = {}
-          keys.each do |key|
-            required_keys_hash[key.to_sym] = ':' + key.to_s
-          end
-          routes_hash[route.to_s] = self.send(route, required_keys_hash)
-        else
-          raise ex
-        end
-      end
-    end
-
-    $log.debug('routes hash passed to javascript is ' + routes_hash.to_s)
-    gon.routes = routes_hash
-  end
-
   def setup_constants
     initialize_isaac_constants #to_do, remove
     $log.debug('term_aux hash passed to javascript is ' + ISAACConstants::TERMAUX.to_s) #to_do, remove
@@ -353,8 +326,6 @@ class KometDashboardController < ApplicationController
     json = YAML.load_file constants_file
     translated_hash = add_translations(json)
     gon.IsaacMetadataAuxiliary = translated_hash
-
-
   end
 
   def metadata
