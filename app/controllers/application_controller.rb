504653b16cbd9e42ab8315ee1fb1aabbbe49da05
gpbowman-git <gpbowman.git@gmail.com>
2016-11-10 12:26:46 -0500

    fixed export_url slashes in gon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -179,7 +179,7 @@ class ApplicationController < ActionController::Base
     clone_hash = ExportRest::VHAT_EXPORT_PATH.clone
     clone_hash[:path] = (PrismeConfigConcern.get_isaac_proxy_context + '/' + clone_hash[:path]).gsub('//','/') if behind_proxy
     gon.vhat_export_params= clone_hash
-    gon.export_url = behind_proxy ? URI(root_url).base_url(true, true) : $PROPS['PRISME.isaac_root']
+    gon.export_url = behind_proxy ? URI(root_url).base_url(true, false) : $PROPS['PRISME.isaac_root']
   end
 
   def pundit_user
