1c91b91b6d9c00d91604cddf72dd691f8e7945b6
cshupp <cshupp@gmail.com>
2016-12-05 11:34:16 -0500

    Add Komet version to Lifecycle up log event

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,9 @@
 context_file = "../context.txt"
 key = File.open(context_file).read.reverse.chop.reverse rescue "_komet_tooling_session"
 context = (key.eql? '_komet_tooling_session') ? '' : key
-$log.always_n(PrismeLogEvent::LIFECYCLE_TAG, "#{Rails.application.class.parent_name}/#{context} coming up!")
+message = "#{Rails.application.class.parent_name}/#{context} coming up!"
+message << "  The version is #{$PROPS['PRISME.war_version']}" if $PROPS['PRISME.war_version']
+$log.always_n(PrismeLogEvent::LIFECYCLE_TAG, message)
 $CONTEXT = (context.empty?) ? '/' : context
 key += '_' + File.mtime(context_file).to_i.to_s rescue ''
 Rails.application.config.session_store :cache_store, key: key
