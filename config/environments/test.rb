6d00caae593e49449307a964c37f41c78f4fe479
cshupp <cshupp@gmail.com>
2017-04-25 15:38:44 -0400

    Giving Komet debugable javascripts.....

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -17,8 +17,8 @@ Rails.application.configure do
   config.static_cache_control = 'public, max-age=3600'
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :sass
+  #config.assets.js_compressor = :uglifier
+  #config.assets.css_compressor = :sass
 
   # Do fallback to assets pipeline if a precompiled asset is missed.
   #config.assets.compile = true
